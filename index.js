453963dc8ab80ea425dc52e9a60c4967bbf0274d
Anirudh <anirudhbalakka@gmail.com>
2020-01-20 09:57:43 +0530

    Refactor to use generator instead of iterator

--- index.js
+++ index.js
@@ -14,41 +14,19 @@ readLine.on('line', async line => {
             const { data } = await axios.get(`http://localhost:3001/food`);
             const it = data[Symbol.iterator]();
             let actionIt;
-            const actionIterator = {
-                [Symbol.iterator]() {
-                    let positions = [...this.actions];
-                    return {
-                        [Symbol.iterator]() {
-                            return this;
-                        },
-                        next(...args) {
-                            if(positions.length > 0) {
-                                const position = positions.shift();
-                                const result = position(...args);
-                                return { value: result, done: false };
-                            } else {
-                                return { done: true };
-                            }
-                        },
-                        return() {
-                            positions = [];
-                            return { done: true};
-                        },
-                        throw (error) {
-                            console.log(error);
-                            return { value: undefined, done: true};
-                        }
-                    };
-                },
-                actions: [askForServingSize, displayCalories],
-            };
+            
+            function *actionGenerator() {
+                const food = yield;
+                const servingSize = askForServingSize = yield askForServingSize();
+                yield displayCalories(servingSize, food);
+            }
 
-            function askForServingSize(food){
+            function askForServingSize(){
                 readLine.question(`How many servings did you eat? `, servingSize => {
                     if( servingSize === 'n') {
                         actionIt.return();
                     } else {
-                        actionIt.next(servingSize, food);
+                        actionIt.next(servingSize);
                     }
                 })    
             };
@@ -88,7 +66,8 @@ readLine.on('line', async line => {
                     const food = position.value.name;
                     if(food === item) {
                         console.log(`${item} has ${position.value.calories} calories`);
-                        actionIt = actionIterator[Symbol.iterator]();
+                        actionIt = actionGenerator();
+                        actionIt.next();
                         actionIt.next(position.value);
                     }
                     position = it.next();
@@ -99,36 +78,58 @@ readLine.on('line', async line => {
         break;
         case 'list vegan foods' :
         {
-            axios.get(`http://localhost:3001/food`).then(({data}) => {
+            const { data } = await axios.get(`http://localhost:3001/food`);
+
+            function *listVeganFoods() {
                 let idx = 0;
                 const veganFood = data.filter(food => {
                     return food.dietary_preferences.includes('vegan');
                 });
 
-                const veganIterable = {
-                    [Symbol.iterator]() {
-                        return {
-                            [Symbol.iterator]() {
-                                return this;
-                            },
-                            next() {
-                                const current = veganFood[idx];
-                                idx++;
-                                if(current)
-                                    return { value: current, done: false}
-                                else
-                                    return { value: current, done: true}
-                            }
-                        }
-                    }
+                while(veganFood[idx]) {
+                    yield veganFood[idx];
+                    idx++;
                 }
-                for(let val of veganIterable) {
+            }
+            
+                for(let val of listVeganFoods()) {
                     console.log(val.name)
                 }
                 readLine.prompt();
-            });
+        }
+        break;
+        case 'todays log':
+        {
+            readLine.question('Email: ', async email => {
+                const { data } = await axios.get(`htpp://localhost:3001/users?email=${email}`);
+                const foodLog = data[0].log || [];
+                let totalCalories = 0;
+
+                function *getFoodLog() {
+                    yield *foodLog;
+                }
+
+                for(const entry of getFoodLog()) {
+                    const timestamp = Object.keys(entry)[0];
+                    if(isToday(new Date(Number(timestamp)))) {
+                        console.log(`${entry[timestamp].food}, ${entry[timestamp].servingSize} servings`);
+                        totalCalories += entry[timestamp].calories;
+                    }
+                }
+                console.log(`\nTodays total calories: ${totalCalories}`);
+                readLine.prompt();
+           })
         }
         break;
     }
-})
+});
+
+const isToday = (timestamp) => {
+    const today = new Date();
+    return (
+        timestamp.getDate() === today.getDate() &&
+        timestamp.getMonth() === today.getMonth() &&
+        timestamp.getFullYear() === today.getFullYear()
+    );
+}
 
